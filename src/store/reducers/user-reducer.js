644c1c15a2805cc289d781ff0ced97917a244c92
abayasaye <abay400@gmail.com>
2022-09-07 14:30:03 +0300

    new

--- src/store/reducers/user-reducer.js
+++ src/store/reducers/user-reducer.js
@@ -1,21 +1,25 @@
-import CREATE_USER from '../actions'
+import CREATE_USER from "../actions/user-action";
+import {
+  ADD_USER,
+  DELETE_USER,
+  GET_BY_ID,
+  UPDATE_USER,
+} from "../types/user-types";
 
-export default function userReducer(state,action){
-    switch (action.type) {
-        case :
-            
-            return;
-            case :
-            
-            return;
-            case :
-            
-            return;
-            case :
-            
-            return;
-    
-        default:
-            return state;
-    }
+export default function userReducer({users}, action) {
+  switch (action.type) {
+    case GET_BY_ID:
+      return [...users];
+    case ADD_USER:
+      return [...users , action.payload];
+    case UPDATE_USER:
+      return users.filter((userItem)=>{
+        
+      });
+    case DELETE_USER:
+      return;
+
+    default:
+      return state;
+  }
 }
