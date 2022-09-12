2f4ae5f877134b6ddabf1797009e0c4621f4c1e2
abayasaye <abay400@gmail.com>
2022-09-12 23:59:52 +0300

    n

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ import './index.css'
 import {createStore,compose} from 'redux'
 import {Provider} from 'react-redux'
 import userReducer from './store/reducers/user-reducer'
-import c ombine from ''
+import combine from ''
 
 const enhancers = compose(
   window._REDUX_DEVTOOLS_EXTENSTION_&&
